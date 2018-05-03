253a378debfda6e6faf6719eeefa9df3d213ef39
Sebastian Sadowski <sadowski.sebastian91@gmail.com>
2018-05-03 19:56:32 +0200

    Sending the project

---
+++ InterviewApp/Loger.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace InterviewApp
+{
+    public static class Loger
+    {
+        #region Properties
+        /// <summary>
+        /// Returns the path of the log file
+        /// </summary>
+        public static string FilePath
+        {
+            get
+            {
+                return $@"{AppDomain.CurrentDomain.BaseDirectory}logs.txt";
+            }
+        }
+
+        /// <summary>
+        /// Returns the name of the EventLog source
+        /// </summary>
+        public static string EventLogSource
+        {
+            get
+            {
+                return "Interview";
+            }
+        }
+
+        /// <summary>
+        /// Returns the path of the Registry log key
+        /// </summary>
+        public static string RegistryKey
+        {
+            get
+            {
+                return @"HKEY_CURRENT_USER\SOFTWARE\InterviewApp\Logs";
+            }
+        }
+        #endregion
+
+        #region SavingMethods
+        ///<summary>
+        ///Adds a log in the EventLog, returns true if adding was successful
+        ///</summary>
+        public static bool LogToEventLog(string message)
+        {
+            try
+            {
+                //checks if the source name exists in the EventLog
+                if (!EventLog.SourceExists(EventLogSource))
+                    EventLog.CreateEventSource(EventLogSource, "Application");
+
+                EventLog.WriteEntry(EventLogSource, message);
+                return true;
+            }
+            //catches administrator permission exception
+            catch (System.Security.SecurityException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {e.Message}");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Possible fix: Start the application with the administrator permission");
+                Console.ResetColor();
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {e.Message}");
+                Console.ResetColor();
+                return false;
+            }
+        }
+
+        ///<summary>
+        ///Adds a log to a file, returns true if adding was successful
+        ///</summary>
+        public static bool LogToFile(string message)
+        {   
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(FilePath, true))
+                {
+                    sw.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} \"{message}\"");
+                    sw.Close();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {e.Message}");
+                Console.ResetColor();
+                return false;
+            }
+        }
+
+        ///<summary>
+        ///Adds a log to the Registry, returns true if adding was successful
+        ///</summary>
+        public static bool LogToRegistry(string message)
+        {
+            try
+            {
+                Registry.SetValue(RegistryKey, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), message);
+               
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {e.Message}");
+                Console.ResetColor();
+                return false;
+            }
+        }
+        #endregion
+
+        #region ReadingMethods
+        /// <summary>
+        /// Displays logs from the file in the console
+        /// </summary>
+        public static void ReadFromFile()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(FilePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {e.Message}");
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+        /// Returns the list of the saved entries in the EventLog
+        /// </summary>
+        public static List<EventLogEntry> ReadFromEventLog()
+        {
+            EventLog eventLog = new EventLog("Application");
+            return eventLog.Entries.Cast<EventLogEntry>()
+                .Where(e => e.Source == EventLogSource).ToList();
+        }
+
+        /// <summary>
+        /// Display logs form the Registry in the console
+        /// </summary>
+        public static void ReadFromRegistry()
+        {
+            RegistryKey rkey = Registry.CurrentUser.OpenSubKey(RegistryKey.Replace(@"HKEY_CURRENT_USER\", ""));
+            if (rkey != null)
+            {
+                var values = rkey.GetValueNames();
+                foreach (var value in values)
+                {
+                    Console.WriteLine($"{value} {rkey.GetValue(value)}");
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Add something to the Registry before reading from it");
+                Console.ResetColor();
+            }
+        }
+        #endregion
+    }
+}
---
+++ InterviewApp/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace InterviewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char option = '0';
+            do
+            {
+                #region Menu
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("InterviewApp");
+                Console.WriteLine("Simple Logger");
+                Console.ResetColor();
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1 - Save a log to the file");
+                Console.WriteLine("2 - Save a log to the EventLog");
+                Console.WriteLine("3 - Save s log to the registry");
+                Console.WriteLine("4 - Read saved entries from the file ");
+                Console.WriteLine("5 - Read saved entries from the EventLog");
+                Console.WriteLine("6 - Read saved entries from the Registry");
+                Console.WriteLine("0 - Exit");
+                char.TryParse(Console.ReadLine(), out option);
+                Console.Clear();
+                #endregion
+
+                switch (option)
+                {
+                    case '1':
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"Adding log to the file, path: {Loger.FilePath}");
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        Console.WriteLine("Write a content of the log");
+
+                        if(Loger.LogToFile(Console.ReadLine()))
+                            Console.WriteLine("Log was added");
+                        else
+                            Console.WriteLine("Adding the log failed");
+
+                        break;
+                    case '2':
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"Adding log to the EventLog as source: {Loger.EventLogSource}");
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        Console.WriteLine("Write a content of the log");
+
+                        if(Loger.LogToEventLog(Console.ReadLine()))
+                            Console.WriteLine("Log was added");
+                        else
+                            Console.WriteLine("Adding the log failed");
+
+                        break;
+                    case '3':
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"Adding log to the Registry, path: {Loger.RegistryKey}");
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        Console.WriteLine("Write a content of the log");
+
+                        if (Loger.LogToRegistry(Console.ReadLine()))
+                            Console.WriteLine("Log was added");
+                        else
+                            Console.WriteLine("Adding the log failed");
+
+                        break;
+                    case '4':
+                        Loger.ReadFromFile();
+
+                        break;
+                    case '5':
+                        List<EventLogEntry> entries = Loger.ReadFromEventLog();
+                        if (entries.Count > 0)
+                        {
+                            foreach (var entry in entries)
+                            {
+                                Console.WriteLine($"{entry.TimeWritten} {entry.Message}");
+                            }
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Add something to the EventLog before reading form it");
+                            Console.ResetColor();
+                        }
+
+                        break;
+                    case '6':
+                        Loger.ReadFromRegistry();
+
+                        break;
+                    case '0':
+                        Console.WriteLine("Exiting...");
+
+                        break;
+                }
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            } while (option != '0');
+        }
+    }
+}
